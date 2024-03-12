8738940055a227d126dba17498af0cf126ee4f2f
Yaroslava <yaroslava.hovenko@gmail.com>
2024-03-12 23:20:10 +0200

    fix some info

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,6 +1,7 @@
 const SearchBox = ({ value, onFilter }) => {
   return (
     <div>
+      <span>Find contacts by name:</span>
       <input type="text" value={value} onChange={(e) => onFilter(e.target.value)} />
     </div>
   );
